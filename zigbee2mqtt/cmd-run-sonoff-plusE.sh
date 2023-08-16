817997e2da9458833d26b5f02257c933472a8d6a
Josh Simonot <JSimonot@live.ca>
2023-08-15 23:44:39 -0400

    revert putting all envar on the line
    it did not resolve the issue:
    
    WARN[0000] The "DEVICE_ID" variable is not set.
    Defaulting to a blank string.
    
    WARN[0000] The "ZIGBEE2MQTT_CONFIG_SERIAL_PORT" variable is not set.
    Defaulting to a blank string.

--- zigbee2mqtt/cmd-run-sonoff-plusE.sh
+++ zigbee2mqtt/cmd-run-sonoff-plusE.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/sh
 echo "configuring for Sonoff dongle plus E"
-ZIGBEE2MQTT_CONFIG_SERIAL_PORT=/dev/ttyUSB0 ZIGBEE2MQTT_CONFIG_ADVANCED_PAN_ID=0xBEEF DEVICE_ID=usb-Silicon_Labs_Sonoff_Zigbee_3.0_USB_Dongle_Plus_0001-if00-port0 docker compose up -d
+export ZIGBEE2MQTT_CONFIG_SERIAL_PORT=/dev/ttyUSB0
+export ZIGBEE2MQTT_CONFIG_ADVANCED_PAN_ID=0xBEEF
+export DEVICE_ID=usb-Silicon_Labs_Sonoff_Zigbee_3.0_USB_Dongle_Plus_0001-if00-port0
+
+echo DEVICE_ID ${DEVICE_ID}
+echo ZIGBEE2MQTT_CONFIG_SERIAL_PORT ${ZIGBEE2MQTT_CONFIG_SERIAL_PORT}
+echo ZIGBEE2MQTT_CONFIG_ADVANCED_PAN_ID ${ZIGBEE2MQTT_CONFIG_ADVANCED_PAN_ID}
+
+docker compose up -d
