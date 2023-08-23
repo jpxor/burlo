4af2b5135cc3faeaf3788c444ab31c9fd1e78bd5
Josh Simonot <JSimonot@live.ca>
2023-08-23 02:46:59 -0400

    Add first draft of mqtt sensor
    paho.mqtt.client doesn't seem to be working at the moment.
    Mosquitto logs show that the client connects, but the on_connect
    callback is never called, and after a few minutes Mosquitto disconnects
    the client saying it exceeded a timeout.  I don't see any errors or
    ret codes from the client

---
+++ controller/sensors.py
@@ -0,0 +1,61 @@
+import json
+import paho.mqtt.client as mqtt
+
+
+class SensorInterface():
+
+    def subscribe(self, callback):
+        if not hasattr(self, "callbacks"):
+            self.callbacks = []
+        self.callbacks.append(callback)
+
+    def publish(self, data):
+        print("publishing")
+        if hasattr(self, "callbacks"):
+            for cb in self.callbacks:
+                cb(data)
+
+
+class MqttSensor(SensorInterface):
+
+    def __init__(self, topic, broker, port=1883):
+        self.topic = topic
+        self.client = mqtt.Client()
+        self.client.on_connect = self.mqtt_on_connect
+        self.client.on_message = self.mqtt_on_message
+        self.client.on_disconnect = self.mqtt_on_disconnect
+        self.client.username_pw_set("hvac", "hvac_pass")
+        self.client.connect(broker, port, 60)
+
+    def mqtt_on_connect(self, client, opaque, flags, rc):
+        print(f'connect rc={rc}, subscribing to "{self.topic}"')
+        client.subscribe(self.topic)
+
+    def mqtt_on_disconnect(self, client, opaque, rc):
+        print(f'Disconnected with result code {str(rc)}, topic "{self.topic}"')
+
+    def mqtt_on_message(self, client, opaque, msg):
+        print(f'recieved message from topic "{msg.topic}"')
+        try:
+            data = json.loads(msg.payload)
+            if "dewpoint" not in data and "temperature" in data and "humidity" in data:
+                if data["humidity"] > 50:
+                    t = data["temperature"]
+                    rh = data["humidity"]
+                    data["dewpoint"] = t - (100-rh) / 5
+            self.publish(data)
+        except:
+            print(f'"mqtt sensor exception:"\
+                  "broker "{self.broker}", topic "{self.topic}", payload {msg.payload}')
+
+
+if __name__ == "__main__":
+    print("testing mqtt enabled sensors")
+    s = MqttSensor(topic="/zigbee2mqtt/thermostats/01/office",
+                   broker="192.168.50.193")
+
+    s.subscribe(callback=lambda data: print(data))
+    s.publish(data={"test": "pubsub"})
+
+    input("Press Enter to stop...")
+    print("done")
