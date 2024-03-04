42d5e7825ec8709661a919ffbf172946967e7ad2
Josh <jsimonot@live.ca>
2024-03-04 00:58:00 -0500

    add query option, hardcode channel and port

--- services/actuators/zone-circulator.py
+++ services/actuators/zone-circulator.py
@@ -6,82 +6,111 @@ import time
 
 
 def onAttach(self):
-	print("Attach!")
-	print(self)
-
-
+    print(f"Attached {self}")
 
 def onDetach(self):
-	print("Detach!")
-	print(self)
-
-
+    print(f"Detached {self}")
 
 def onError(self, code, description):
-	print("Code: " + ErrorEventCode.getName(code))
-	print("Description: " + str(description))
-	print("----------")
+    print("Code: " + ErrorEventCode.getName(code))
+    print("Description: " + str(description))
+    print("----------")
 
 
 
 def enable():
-	try:
-		digitalOutput0 = DigitalOutput()
-		digitalOutput0.setOnAttachHandler(onAttach)
-		digitalOutput0.setOnDetachHandler(onDetach)
-		digitalOutput0.setOnErrorHandler(onError)
+    try:
+        digitalOutput0 = DigitalOutput()
+        digitalOutput0.setChannel(0)
+        digitalOutput0.setHubPort(0)
 
-        # specify hub,port,etc
-		digitalOutput0.openWaitForAttachment(5000)
-		digitalOutput0.setDutyCycle(1)
+        digitalOutput0.setOnAttachHandler(onAttach)
+        digitalOutput0.setOnDetachHandler(onDetach)
+        digitalOutput0.setOnErrorHandler(onError)
 
-	except PhidgetException as ex:
-		traceback.print_exc()
-		print("")
-		print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
+        digitalOutput0.openWaitForAttachment(5000)
+        digitalOutput0.setDutyCycle(1)
+        digitalOutput0.close()
+
+    except PhidgetException as ex:
+        traceback.print_exc()
+        print("")
+        print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
 
 
 
 def disable():
-	try:
-		digitalOutput0 = DigitalOutput()
-		digitalOutput0.setOnAttachHandler(onAttach)
-		digitalOutput0.setOnDetachHandler(onDetach)
-		digitalOutput0.setOnErrorHandler(onError)
+    try:
+        digitalOutput0 = DigitalOutput()
+        digitalOutput0.setChannel(0)
+        digitalOutput0.setHubPort(0)
 
-        # specify hub,port,etc
-		digitalOutput0.openWaitForAttachment(5000)
-		digitalOutput0.setDutyCycle(0)
+        digitalOutput0.setOnAttachHandler(onAttach)
+        digitalOutput0.setOnDetachHandler(onDetach)
+        digitalOutput0.setOnErrorHandler(onError)
 
-	except PhidgetException as ex:
-		traceback.print_exc()
-		print("")
-		print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
+        digitalOutput0.openWaitForAttachment(5000)
+        digitalOutput0.setDutyCycle(0)
+        digitalOutput0.close()
+
+    except PhidgetException as ex:
+        traceback.print_exc()
+        print("")
+        print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
 
 
 
 def test():
-	try:
-		digitalOutput0 = DigitalOutput()
-		digitalOutput0.setOnAttachHandler(onAttach)
-		digitalOutput0.setOnDetachHandler(onDetach)
-		digitalOutput0.setOnErrorHandler(onError)
+    try:
+        digitalOutput0 = DigitalOutput()
+        digitalOutput0.setChannel(0)
+        digitalOutput0.setHubPort(0)
 
-        # specify hub,port,etc
-		digitalOutput0.openWaitForAttachment(5000)
-		digitalOutput0.setDutyCycle(1)
-		time.sleep(5)
-		digitalOutput0.close()
+        digitalOutput0.setOnAttachHandler(onAttach)
+        digitalOutput0.setOnDetachHandler(onDetach)
+        digitalOutput0.setOnErrorHandler(onError)
 
-	except PhidgetException as ex:
-		traceback.print_exc()
-		print("")
-		print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
+        digitalOutput0.openWaitForAttachment(5000)
+        digitalOutput0.setDutyCycle(1)
+        time.sleep(5)
+        digitalOutput0.setDutyCycle(0)
+        digitalOutput0.close()
+
+    except PhidgetException as ex:
+        traceback.print_exc()
+        print("")
+        print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
+
+
+
+def query():
+    try:
+        do = DigitalOutput()
+        do.setChannel(0)
+        do.setHubPort(0)
+
+        do.setOnAttachHandler(onAttach)
+        do.setOnDetachHandler(onDetach)
+        do.setOnErrorHandler(onError)
+
+        do.openWaitForAttachment(5000)
+        print(f"getDutyCycle {do.getDutyCycle()}")
+        print(f"getChannel {do.getChannel()}")
+        print(f"getHubPort {do.getHubPort()}")
+        print(f"getHub {do.getHub()}")
+        print(f"getDeviceSerialNumber {do.getDeviceSerialNumber()}")
+        do.close()
+
+    except PhidgetException as ex:
+        traceback.print_exc()
+        print("")
+        print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
+
 
 
 # get the args
 if len(sys.argv) != 2:
-    print("Usage: zone-circulator.py [enable|disable|test]")
+    print("Usage: zone-circulator.py [enable|disable|test|query]")
     sys.exit(1)
 
 cmd = sys.argv[1]
@@ -92,6 +121,8 @@ elif cmd == "disable":
     disable()
 elif cmd == "test":
     test()
+elif cmd == "query":
+    query()
 else:
     print("Invalid command. Must be enable, disable or test.")
     sys.exit(1)
