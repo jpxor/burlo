d3d9ee917ec91211ec985b5f43b17e347f46a272
Josh <jsimonot@live.ca>
2024-03-04 10:19:09 -0500

    - update imports to fix module error - fix http handler responses - add test function

--- services/actuators/phidgets.py
+++ services/actuators/phidgets.py
@@ -2,9 +2,12 @@ from aiohttp import web
 
 from Phidget22.Phidget import *
 from Phidget22.PhidgetException import *
+from Phidget22.Devices.DigitalOutput import *
 
+import sys
+import aiohttp
+import asyncio
 import traceback
-import Phidget22.Devices.DigitalOutput as DigitalOutput
 
 ## active phidget channels
 named_phidgets = {}
@@ -38,22 +41,23 @@ def onError(self, code, description):
 
 async def set_digital_output(request):
     data = await request.json()
+    print("set_digital_output", data)
     
     name = data['name']
     if not isinstance(name, str):
-        return 400, "name must be a string"
+        return web.Response(status=400, text="name must be a string")
 
     channel = data['channel']
     if not isinstance(channel, int):
-        return 400, "channel must be an integer"
+        return web.Response(status=400, text="channel must be an integer")
 
     hub_port = data['hub_port'] 
     if not isinstance(hub_port, int):
-        return 400, "hub_port must be an integer"
+        return web.Response(status=400, text="hub_port must be an integer")
 
     target_state = data['target_state']
     if not isinstance(target_state, bool):
-        return 400, "target_state must be a boolean"
+        return web.Response(status=400, text="target_state must be a boolean")
     
     phiwrap = named_phidgets.get(name)
     if not phiwrap:
@@ -72,8 +76,7 @@ async def set_digital_output(request):
 
         except PhidgetException as ex:
             traceback.print_exc()
-            print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
-            return web.Response(status=500)
+            return web.Response(status=500, text=str(ex))
 
     phiwrap.phidget.setState(target_state)
     return web.Response(status=200)
@@ -81,16 +84,18 @@ async def set_digital_output(request):
 
 async def detach_phidget_channel(request):
     data = await request.json()
+    print("detach_phidget_channel", data)
     
     name = data['name']
     if not isinstance(name, str):
-        return 400, "name must be a string"
+        return web.Response(status=400, text="name must be a string")
 
     try:
         phidget = named_phidgets[name].phidget
         await phidget.close()
-    except Exception as e:
-        raise web.HTTPBadRequest(reason=str(e))
+    except Exception as ex:
+        traceback.print_exc()
+        raise web.HTTPBadRequest(reason=str(ex))
 
 
 async def get_phidgets_state(request):
@@ -99,11 +104,47 @@ async def get_phidgets_state(request):
 
 app = web.Application()
 app.add_routes([
-    web.post('/phidgets/digital_out/', set_digital_output),
-    web.post('/phidgets/detach/', detach_phidget_channel),
+    web.post('/phidgets/digital_out', set_digital_output),
+    web.post('/phidgets/detach', detach_phidget_channel),
     web.get('/phidgets/state', get_phidgets_state),
 ])
 
 
-if __name__ == '__main__':
+def run():
     web.run_app(app, port=4000)
+
+
+def test():
+    async def test_phidgets():
+        async with aiohttp.ClientSession() as session:
+            # Test setting digital output
+            data = {'name': 'my_device', 'channel': 0, 'hub_port': 0, 'target_state': 2}
+            async with session.post('http://192.168.50.193:4000/phidgets/digital_out/', json=data) as resp:
+                print(resp.status, resp.reason)
+            
+            # Test getting state
+            async with session.get('http://192.168.50.193:4000/phidgets/state') as resp:
+                print(await resp.json())
+                
+            # Test detaching
+            data = {'name': 'my_device'}
+            async with session.post('http://192.168.50.193:4000/phidgets/detach/', json=data) as resp:
+                print(resp.status)
+                
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test_phidgets())
+
+
+
+if len(sys.argv) != 2:
+    print("Usage: phidgets.py [test|run]")
+    sys.exit(1)
+    
+cmd = sys.argv[1]
+if cmd == "test":
+    test()
+elif cmd == "run":
+    run()
+else:
+    print("Invalid argument. Usage: phidgets.py [test|run]")
+
