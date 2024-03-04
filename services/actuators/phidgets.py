1438d679f1e64664f00939f821b237384d2efb56
Josh <jsimonot@live.ca>
2024-03-04 02:16:57 -0500

    add phidget actuator server
    the one-shot py script did not work because all phidget devices were stopped as
    soon as the script exited.  So replacing it with an http server to keep all connections
    alive

---
+++ services/actuators/phidgets.py
@@ -0,0 +1,109 @@
+from aiohttp import web
+
+from Phidget22.Phidget import *
+from Phidget22.PhidgetException import *
+
+import traceback
+import Phidget22.Devices.DigitalOutput as DigitalOutput
+
+## active phidget channels
+named_phidgets = {}
+
+class NamedPhidget:
+    def __init__(self, name, phidget):
+        self.phidget = phidget
+        self.name = name
+
+
+def name_from_phidget(phidget):
+    for name, wrapper in named_phidgets.items():
+        if phidget == wrapper.phidget:
+            return name
+
+
+def onAttach(self):
+    print(f"Attached: {self}")
+
+
+def onDetach(self):
+    print(f"Detached: {self}")
+    del named_phidgets[name_from_phidget(self)]
+
+
+def onError(self, code, description):
+    print("Code: " + ErrorEventCode.getName(code))
+    print("Description: " + str(description))
+    print("----------")
+
+
+async def set_digital_output(request):
+    data = await request.json()
+    
+    name = data['name']
+    if not isinstance(name, str):
+        return 400, "name must be a string"
+
+    channel = data['channel']
+    if not isinstance(channel, int):
+        return 400, "channel must be an integer"
+
+    hub_port = data['hub_port'] 
+    if not isinstance(hub_port, int):
+        return 400, "hub_port must be an integer"
+
+    target_state = data['target_state']
+    if not isinstance(target_state, bool):
+        return 400, "target_state must be a boolean"
+    
+    phiwrap = named_phidgets.get(name)
+    if not phiwrap:
+        try:
+            do = DigitalOutput()
+            do.setChannel(channel)
+            do.setHubPort(hub_port)
+            
+            do.setOnAttachHandler(onAttach)
+            do.setOnDetachHandler(onDetach)
+            do.setOnErrorHandler(onError)
+            do.openWaitForAttachment(5000)
+
+            phiwrap = NamedPhidget(name, do)
+            named_phidgets[name] = phiwrap
+
+        except PhidgetException as ex:
+            traceback.print_exc()
+            print("PhidgetException " + str(ex.code) + " (" + ex.description + "): " + ex.details)
+            return web.Response(status=500)
+
+    phiwrap.phidget.setState(target_state)
+    return web.Response(status=200)
+
+
+async def detach_phidget_channel(request):
+    data = await request.json()
+    
+    name = data['name']
+    if not isinstance(name, str):
+        return 400, "name must be a string"
+
+    try:
+        phidget = named_phidgets[name].phidget
+        await phidget.close()
+    except Exception as e:
+        raise web.HTTPBadRequest(reason=str(e))
+
+
+async def get_phidgets_state(request):
+    return web.json_response(named_phidgets)
+
+
+app = web.Application()
+app.add_routes([
+    web.post('/phidgets/digital_out/', set_digital_output),
+    web.post('/phidgets/detach/', detach_phidget_channel),
+    web.get('/phidgets/state', get_phidgets_state),
+])
+
+
+if __name__ == '__main__':
+    web.run_app(app, port=4000)
