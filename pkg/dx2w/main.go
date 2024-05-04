1876f71ed3396d4858517a579d4a983d001925b9
Josh <jsimonot@live.ca>
2024-05-04 09:26:21 -0400

    convert to lib pkg
    this is now a lib, not a runnable service

--- pkg/dx2w/main.go
+++ pkg/dx2w/main.go
@@ -1,45 +1,10 @@
-package main
+package dx2w
 
 import (
-	"context"
 	_ "embed"
-	"flag"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
-	"time"
 )
 
 //go:embed dx2w-modbus.toml
 var modbusConf []byte
 
-type global_vars struct {
-	waitgroup sync.WaitGroup
-}
-
-var global = global_vars{}
-
-func main() {
-
-	printRegs := flag.Bool("print", false, "[debug] just print all register values")
-	flag.Parse()
-
-	cfg := ParseConfig(modbusConf)
-
-	if *printRegs {
-		printRegisters(cfg)
-		os.Exit(0)
-	}
-
-	// start services
-	cache := NewAutoCache(cfg, 20*time.Second)
-	defer cache.Stop()
-
-	cache.Subscribe(init_energy_monitor())
-
-	// wait for signal before exiting
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-	<-ctx.Done()
-}
+var cfg = ParseConfig(modbusConf)
