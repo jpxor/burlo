36ea2a4b0604feb4b171cba935804e873473f90b
Josh <jsimonot@live.ca>
2024-05-05 15:33:59 -0400

    removed unused file

--- pkg/dx2w/energy_monitor.go
+++
@@ -1,9 +0,0 @@
-package dx2w
-
-func write_to_influxdb() {
-	// TODO: create config for influxdb settings
-	// The following api key is safe to post publicly
-	// client := influxdb2.NewClient("http://192.168.50.2:8086",
-	// 	"ydFe9rktND-euJoC-FdvU2OO98AnNYroGzU4cEJOmq5un9p_gbgCBIw-vun1ZcMu3GOiR-UXqB20tXDIzpJ2yg==")
-	// writer := client.WriteAPIBlocking("home", "power_monitor")
-}
