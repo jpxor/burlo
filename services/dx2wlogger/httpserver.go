56a24294ebc289a8ac684fb95482bb3345de8a70
Josh Simonot <JSimonot@live.ca>
2024-05-09 02:16:19 -0400

    remove GetPageUI handler
    this is handled in a different service

--- services/dx2wlogger/httpserver.go
+++ services/dx2wlogger/httpserver.go
@@ -30,7 +30,6 @@ func http_server(port string) {
 	}()
 
 	mux.HandleFunc("GET /dx2w/registers", GetRegisters())
-	mux.HandleFunc("GET /dx2w", GetPageUI())
 	mux.HandleFunc("/", CatchAll())
 
 	log.Println("http_server started, port:", port)
@@ -43,27 +42,11 @@ func http_server(port string) {
 
 func CatchAll() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/dx2w", http.StatusFound)
+		http.Redirect(w, r, "/dx2w/registers", http.StatusFound)
 	}
 }
 
 func GetRegisters() http.HandlerFunc {
-	jsonBytes := func(data interface{}) []byte {
-		json, err := json.MarshalIndent(data, "", "    ")
-		if err != nil {
-			return []byte(err.Error())
-		}
-		return json
-	}
-	return func(w http.ResponseWriter, r *http.Request) {
-		global_mutex.Lock()
-		defer global_mutex.Unlock()
-		w.Write(jsonBytes(register_map))
-	}
-}
-
-// TODO: build html page
-func GetPageUI() http.HandlerFunc {
 	jsonBytes := func(data interface{}) []byte {
 		json, err := json.MarshalIndent(data, "", "    ")
 		if err != nil {
